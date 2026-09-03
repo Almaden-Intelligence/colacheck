6b7d2c66b5c1a5f56274d47dd0941e3d2422cc0a
gerardogrivera <gerardogrivera@icloud.com>
2026-09-03 16:54:55 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@anthropic-ai/sdk'],
+    outputFileTracingIncludes: {
+      '/api/check': ['./lib/cfr/**/*.md'],
+    },
   },
 }
 
